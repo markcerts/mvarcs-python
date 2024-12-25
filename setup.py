f8648618140a3e118bf88fe700b1f559af4eb0b6
OllieJC <5426038+OllieJC@users.noreply.github.com>
2024-12-25 16:53:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mvarcs",
-    version="0.1.0",
+    version="0.1.1",
     author="OllieJC",
     author_email="mvarcs-pypi@olliejc.uk",
-    description="Python package for providing the MVARCS",
+    description="Python package for providing the MVA Root Certificates Store",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/markcerts/mvarcs-python",
@@ -25,6 +25,13 @@ setuptools.setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
     ],
+    keywords = [
+        "BIMI",
+        "VMC",
+        "CMC",
+        "Verification",
+        "Trusted",
+    ]
     package_dir={"mvarcs": "src"},
     package_data={"mvarcs": ["mvarcs/*.pem"]},
     packages=["mvarcs"],
