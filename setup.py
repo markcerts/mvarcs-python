0b6562ca75bc8f732e212c13df98095827c0dc6e
OllieJC <5426038+OllieJC@users.noreply.github.com>
2024-12-25 08:35:09 +0000

    Move to beta and set version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mvarcs",
-    version="0.0.2",
+    version="0.1.0",
     author="OllieJC",
     author_email="mvarcs-pypi@olliejc.uk",
     description="Python package for providing the MVARCS",
@@ -18,7 +18,7 @@ setuptools.setup(
     },
     license="The Unlicense",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: The Unlicense (Unlicense)",
         "Programming Language :: Python :: 3",
